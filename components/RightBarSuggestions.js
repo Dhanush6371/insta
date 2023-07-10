5ad2d4ca792a745ebd426369192eb02e74846b69
Dhanush6371 <98277180+Dhanush6371@users.noreply.github.com>
2023-07-10 11:58:43 +0530

    Update RightBarSuggestions.js

--- components/RightBarSuggestions.js
+++ components/RightBarSuggestions.js
@@ -6,7 +6,6 @@ const style = {
     titleContainer: `suggestions-header flex mt-[.8rem]`,
     title: `text-14-bold mr-auto text-[#8e8e8e]`,
 }
-
 const RightBarSuggestions = ({ data }) => {
     return (
         <div className={style.wrapper}>
