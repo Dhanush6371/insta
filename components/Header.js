0be4d21ccc1c00be6c8b41df76b45c46a91b3ce5
Dhanush6371 <98277180+Dhanush6371@users.noreply.github.com>
2023-07-10 11:56:05 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,6 @@
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import Modal from 'react-modal'
-
-
-
 import { GrHomeRounded } from 'react-icons/gr'
 import { AiOutlineCloudUpload } from 'react-icons/ai'
 import { IoPaperPlaneOutline } from 'react-icons/io5'
