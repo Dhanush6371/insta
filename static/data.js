353c302057629a357e44473b48f85b8e116f93fa
Dhanush6371 <98277180+Dhanush6371@users.noreply.github.com>
2023-07-10 12:05:11 +0530

    Update data.js

--- static/data.js
+++ static/data.js
@@ -1,15 +1,6 @@
 export const data = {
     stories: [
-        {
-            username: 'cleverqazi',
-            image:
-                'https://pbs.twimg.com/profile_images/1492902736592969732/asuDqTgR_400x400.jpg',
-        },
-        {
-            username: 'cp',
-            image:
-                'https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png',
-        },
+        
         {
             username: 'zuck',
             image:
@@ -32,18 +23,7 @@ export const data = {
         },
     ],
     suggestions: [
-        {
-            username: 'cleverqazi',
-            image:
-                'https://pbs.twimg.com/profile_images/1492902736592969732/asuDqTgR_400x400.jpg',
-            text: 'Followed by testuser + 16 more',
-        },
-        {
-            username: 'cleverprogrammer',
-            image:
-                'https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png',
-            text: 'Followed by cleverqazi + 22 more',
-        },
+        
         {
             username: 'zuck',
             image:
